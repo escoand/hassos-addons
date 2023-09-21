7775ba1b7aaeb21882d688e0c64e1af6f4100cf4
escoand <escoand@users.noreply.github.com>
2023-09-21 23:51:52 +0200

    extend samba addon

--- samba/run.sh
+++ samba/run.sh
@@ -1,30 +1,54 @@
 #!/usr/bin/with-contenv bashio
 
-set -e 
+set -e
 
 # gobal
-HOSTNAME=$(
-  wget -O- --header "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/info |
-  jq -r .data.hostname
-)
+USER_=$(bashio::config user root)
+HOSTNAME=$(bashio::info.hostname)
 {
 echo "[global]"
-echo "guest account = root"
-echo "log file = /dev/stdout"
+echo "log level = 2"
 echo "netbios name = $HOSTNAME"
+echo "security = user"
+echo "ntlm auth = yes"
+echo "load printers = no"
+echo "disable spoolss = yes"
 } > /etc/samba/smb.conf
 
-SHARES=$(bashio::config 'shares')
-if [ "$SHARES" != null ]; then
-  echo "$SHARES" |
-  while read NAME PATH; do
-    echo "[$NAME]"
-    echo "browseable = yes"
+CNT=0
+while $(bashio::config.exists "shares[$CNT]"); do
+  NAME=$(bashio::config "shares[$CNT].name")
+  PATH_=$(bashio::config "shares[$CNT].path")
+  USERS=$(bashio::config "shares[$CNT].users")
+  WRITABLE=$(bashio::config "shares[$CNT].writable" no)
+
+  echo "[$NAME]"
+  echo "force user = $USER_"
+  echo "force group = $USER_"
+  echo "browseable = yes"
+  echo "path = $PATH_"
+  echo "writable = $WRITABLE"
+  if [ "$USERS" != null ]; then
+    echo "valid users = $USERS"
+  else
     echo "guest only = yes"
-    echo "path = $PATH"
-    echo "read only = yes"
-  done >> /etc/samba/smb.conf
-fi
- 
-/usr/sbin/nmbd -D
-exec /usr/sbin/smbd -F
+  fi
+
+  CNT=$((CNT+1))
+done >> /etc/samba/smb.conf
+
+CNT=0
+while $(bashio::config.exists "users[$CNT]"); do
+  USERNAME=$(bashio::config "users[$CNT].username")
+  PASSWD=$(bashio::config "users[$CNT].password")
+
+  addgroup "$USERNAME"
+  adduser -D -H -G "$USERNAME" -s /bin/false "$USERNAME"
+  printf '%s\n%s\n' "$PASSWD" "$PASSWD" |
+  smbpasswd -a -s -c /etc/samba/smb.conf "$USERNAME"
+
+  CNT=$((CNT+1))
+done
+
+/usr/sbin/nmbd --foreground --debug-stdout --no-process-group &
+exec /usr/sbin/smbd --foreground --debug-stdout --no-process-group
