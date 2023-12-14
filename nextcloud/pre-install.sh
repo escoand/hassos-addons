7476e9023dec8152777214f8bfd9a050f8d4773c
vuubd5u <andreas.schoenfelder@vwgis.de>
2023-12-14 09:21:15 +0100

    test nextcloud image

---
+++ nextcloud/pre-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -s /var/www/html/config/config.php ]; then
+    echo '<?php $CONFIG=array("localstorage.allowsymlinks"=>true);' > /var/www/html/config/config.php
+fi
--- nextcloud/pre-start.sh
+++ nextcloud/pre-start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+set -e
+
 if [ -s /var/www/html/config/config.php ]; then
+    php /var/www/html/occ config:system:delete localstorage.allowsymlinks
     php /var/www/html/occ upgrade
     php /var/www/html/occ maintenance:update:htaccess
 fi
