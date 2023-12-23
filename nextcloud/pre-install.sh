b33d2db9ad8f424db282ecf1d0b4cb8b3e10ec06
escoand <escoand@users.noreply.github.com>
2023-12-23 15:44:59 +0100

    avoid buildkit

--- nextcloud/pre-install.sh
+++ nextcloud/pre-install.sh
@@ -3,5 +3,5 @@
 set -e
 
 if [ ! -s /var/www/html/config/config.php ]; then
-    echo '<?php $CONFIG=array("localstorage.allowsymlinks"=>true);' > /var/www/html/config/config.php
+    echo '<?php $CONFIG=array("localstorage.allowsymlinks"=>true);' >/var/www/html/config/config.php
 fi
