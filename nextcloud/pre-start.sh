90ce20e8540f78d4484076c7f5793310ee287e21
escoand <escoand@users.noreply.github.com>
2024-06-19 21:56:37 +0200

    restore pre-start

--- nextcloud/pre-start.sh
+++ nextcloud/pre-start.sh
@@ -4,4 +4,6 @@ set -eu
 
 if [ -s /var/www/html/config/config.php ] && grep -q "'installed'[[:blank:]]*=>[[:blank:]]*true" /var/www/html/config/config.php ; then
     php /var/www/html/occ config:system:delete localstorage.allowsymlinks
+    php /var/www/html/occ upgrade
+    php /var/www/html/occ maintenance:update:htaccess
 fi
