2c42a929ddf1ee3b59d2d4474cd3ecb0f7192864
escoand <escoand@users.noreply.github.com>
2025-02-13 14:06:47 +0100

    add ml url config

--- immich/run.sh
+++ immich/run.sh
@@ -12,6 +12,8 @@ DB_PASSWORD=$(jq -r '.database_password // empty' $CONFIG_PATH)
 [ -n "$DB_PASSWORD" ] && export DB_PASSWORD
 DB_USERNAME=$(jq -r '.database_user // empty' $CONFIG_PATH)
 [ -n "$DB_USERNAME" ] && export DB_USERNAME
+IMMICH_MACHINE_LEARNING_URL=$(jq -r '.ml_url // empty' $CONFIG_PATH)
+[ -n "$IMMICH_MACHINE_LEARNING_URL" ] && export IMMICH_MACHINE_LEARNING_URL
 REDIS_HOSTNAME=$(jq -r '.redis_host // empty' $CONFIG_PATH)
 [ -n "$REDIS_HOSTNAME" ] && export REDIS_HOSTNAME
 REDIS_PORT=$(jq -r '.redis_port // empty' $CONFIG_PATH)
